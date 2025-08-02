8f3b1181014bfd640875ae62250005c7b2163025
unknown <sahanianchal7@gmail.com>
2025-08-02 11:36:15 +0530

    responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Scroll Spy
+
 window.addEventListener('scroll', () => {
   const sections = document.querySelectorAll('section[id]');
   const navLinks = document.querySelectorAll('.nav-links a');
@@ -39,4 +39,13 @@ const revealObserver = new IntersectionObserver(entries => {
 reveals.forEach((el, index) => {
   el.style.transitionDelay = `${index * 0.2}s`;
   revealObserver.observe(el);
+});
+// script.js
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
