b52e773398a0cf53da2b6555a156b8426498eaa2
unknown <sahanianchal7@gmail.com>
2025-08-08 02:00:02 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -9,12 +11,29 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main>
+      {/* Crisp Chat Widget */}
+      <Script
+        strategy="afterInteractive"
+        src="https://client.crisp.chat/l.js"
+      />
+      <Script
+        id="crisp-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.$crisp = [];
+            window.CRISP_WEBSITE_ID = "https://advikaclinic.vercel.app/";
+          `,
+        }}
+      />
+
+      {/* Page Content */}
       <Navbar />
       <Hero />
       <Services />
       <AboutSection />
       <ServiceList />
-      <Testimonials />  
+      <Testimonials />
       <Footer />
     </main>
   );
